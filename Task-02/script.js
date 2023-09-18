c1a0398fbcca3c826edb6b47d8d78e68665774f2
Himanshu Menghani <91124848+524himanshu@users.noreply.github.com>
2023-09-18 21:08:54 +0530

    update

--- Task-02/script.js
+++ Task-02/script.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const taskInput = document.getElementById("task-input");
+    const deadlineInput = document.getElementById("deadline-input");
+    const priorityInput = document.getElementById("priority-input");
+    const labelInput = document.getElementById("label-input");
     const addTaskButton = document.getElementById("add-task");
     const taskList = document.getElementById("task-list");
 
     addTaskButton.addEventListener("click", function () {
         const taskText = taskInput.value.trim();
+        const deadline = deadlineInput.value;
+        const priority = priorityInput.value;
+        const labels = labelInput.value.split(',');
 
         if (taskText !== "") {
             const listItem = document.createElement("li");
             listItem.innerHTML = `
                 <span>${taskText}</span>
-                <button class="delete-button">Delete</button>
+                <span>Deadline: ${deadline}</span>
+                <span>Priority: ${priority}</span>
+                <span>Labels: ${labels.join(', ')}</span>
             `;
 
             taskList.appendChild(listItem);
             taskInput.value = "";
-
-            // Add event listener to delete button
-            const deleteButton = listItem.querySelector(".delete-button");
-            deleteButton.addEventListener("click", function () {
-                listItem.remove();
-            });
+            deadlineInput.value = "";
+            priorityInput.value = "low";
+            labelInput.value = "";
         }
     });
 });
